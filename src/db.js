9e6c8e4937252697adfba26c1abb3dac994c21d3
Alex Messias <alexmessias_18@yahoo.com.br>
2026-09-22 10:20:40 -0300

    Refactor db.js to use constants and improve readability

--- src/db.js
+++ src/db.js
@@ -1,54 +1,61 @@
-const DB_NAME = "parking-db";
-const STORE = "vehicles";
+const DB_NAME = 'parking-db'
+const STORE_NAME = 'vehicles'
+const DB_VERSION = 1
 
 function openDB() {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, 1);
+    const request = indexedDB.open(DB_NAME, DB_VERSION)
 
     request.onupgradeneeded = () => {
-      const db = request.result;
-      if (!db.objectStoreNames.contains(STORE)) {
-        db.createObjectStore(STORE, { keyPath: "id" });
-      }
-    };
+      const db = request.result
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject(request.error);
-  });
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: 'id' })
+      }
+    }
+
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
 }
 
 export async function getVehicles() {
-  const db = await openDB();
+  const db = await openDB()
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE, "readonly");
-    const request = transaction.objectStore(STORE).getAll();
+    const transaction = db.transaction(STORE_NAME, 'readonly')
+    const store = transaction.objectStore(STORE_NAME)
+    const request = store.getAll()
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = () => reject(request.error);
-  });
+    request.onsuccess = () => resolve(request.result)
+    request.onerror = () => reject(request.error)
+  })
 }
 
 export async function saveVehicle(vehicle) {
-  const db = await openDB();
+  const db = await openDB()
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE, "readwrite");
-    transaction.objectStore(STORE).put(vehicle);
+    const transaction = db.transaction(STORE_NAME, 'readwrite')
+    const store = transaction.objectStore(STORE_NAME)
 
-    transaction.oncomplete = resolve;
-    transaction.onerror = () => reject(transaction.error);
-  });
+    store.put(vehicle)
+
+    transaction.oncomplete = () => resolve()
+    transaction.onerror = () => reject(transaction.error)
+  })
 }
 
 export async function deleteVehicle(id) {
-  const db = await openDB();
+  const db = await openDB()
 
   return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE, "readwrite");
-    transaction.objectStore(STORE).delete(id);
+    const transaction = db.transaction(STORE_NAME, 'readwrite')
+    const store = transaction.objectStore(STORE_NAME)
 
-    transaction.oncomplete = resolve;
-    transaction.onerror = () => reject(transaction.error);
-  });
+    store.delete(id)
+
+    transaction.oncomplete = () => resolve()
+    transaction.onerror = () => reject(transaction.error)
+  })
 }
