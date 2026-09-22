d4e2bc8ab03aef292411ffadb153ca933f430a12
Alex Messias <alexmessias_18@yahoo.com.br>
2026-09-22 10:53:02 -0300

    Refactor openDB and vehicle functions for clarity

--- src/db.js
+++ src/db.js
@@ -4,58 +4,123 @@ const DB_VERSION = 1
 
 function openDB() {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open(DB_NAME, DB_VERSION)
+    const request = indexedDB.open(
+      DB_NAME,
+      DB_VERSION,
+    )
 
     request.onupgradeneeded = () => {
       const db = request.result
 
-      if (!db.objectStoreNames.contains(STORE_NAME)) {
-        db.createObjectStore(STORE_NAME, { keyPath: 'id' })
+      if (
+        !db.objectStoreNames.contains(
+          STORE_NAME,
+        )
+      ) {
+        db.createObjectStore(
+          STORE_NAME,
+          {
+            keyPath: 'id',
+          },
+        )
       }
     }
 
-    request.onsuccess = () => resolve(request.result)
-    request.onerror = () => reject(request.error)
+    request.onsuccess = () => {
+      resolve(request.result)
+    }
+
+    request.onerror = () => {
+      reject(request.error)
+    }
   })
 }
 
 export async function getVehicles() {
   const db = await openDB()
 
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE_NAME, 'readonly')
-    const store = transaction.objectStore(STORE_NAME)
-    const request = store.getAll()
+  return new Promise(
+    (resolve, reject) => {
+      const transaction =
+        db.transaction(
+          STORE_NAME,
+          'readonly',
+        )
 
-    request.onsuccess = () => resolve(request.result)
-    request.onerror = () => reject(request.error)
-  })
+      const store =
+        transaction.objectStore(
+          STORE_NAME,
+        )
+
+      const request = store.getAll()
+
+      request.onsuccess = () => {
+        resolve(request.result)
+      }
+
+      request.onerror = () => {
+        reject(request.error)
+      }
+    },
+  )
 }
 
-export async function saveVehicle(vehicle) {
+export async function saveVehicle(
+  vehicle,
+) {
   const db = await openDB()
 
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE_NAME, 'readwrite')
-    const store = transaction.objectStore(STORE_NAME)
+  return new Promise(
+    (resolve, reject) => {
+      const transaction =
+        db.transaction(
+          STORE_NAME,
+          'readwrite',
+        )
 
-    store.put(vehicle)
+      const store =
+        transaction.objectStore(
+          STORE_NAME,
+        )
 
-    transaction.oncomplete = () => resolve()
-    transaction.onerror = () => reject(transaction.error)
-  })
+      store.put(vehicle)
+
+      transaction.oncomplete = () => {
+        resolve()
+      }
+
+      transaction.onerror = () => {
+        reject(transaction.error)
+      }
+    },
+  )
 }
 
 export async function deleteVehicle(id) {
   const db = await openDB()
 
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction(STORE_NAME, 'readwrite')
-    const store = transaction.objectStore(STORE_NAME)
+  return new Promise(
+    (resolve, reject) => {
+      const transaction =
+        db.transaction(
+          STORE_NAME,
+          'readwrite',
+        )
 
-    store.delete(id)
+      const store =
+        transaction.objectStore(
+          STORE_NAME,
+        )
 
-    transaction.oncomplete = () => resolve()
-    transaction.onerror = () => reject(transaction.error)
-  })
+      store.delete(id)
+
+      transaction.oncomplete = () => {
+        resolve()
+      }
+
+      transaction.onerror = () => {
+        reject(transaction.error)
+      }
+    },
+  )
 }
